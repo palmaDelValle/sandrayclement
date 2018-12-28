301446b886db53c86eb80a4cc2b797d7e7791e8c
Javier Palma del Valle <francisco.palma.v@ocu.es>
2018-12-28 20:17:28 +0100

    Se añade el multidioma

---
+++ js/page-translation.js
@@ -0,0 +1,58 @@
+var linguJSON = {
+    "languages": [
+        {
+            "lang_name": "<img src=\"../img/united-kingdom.png\">",
+            "lang_code": "en",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "<img src=\"../img/spain.png\">",
+            "lang_code": "es",
+            "url_pattern": "?"
+        }
+    ],
+    "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a></div>",
+    "custom_lang_switcher_container_id": "langlinks",
+    "translated_segments": [
+        {
+            "source": "About us",
+            "target": "Nosotros"
+        },
+        {
+            "source": "Gallery",
+            "target": "Galería"
+        },
+        {
+            "source": "Form",
+            "target": "Formulario"
+        },
+        {
+            "source": "are Getting Married",
+            "target": "se casan"
+        },
+        {
+            "source": "Tell us everthing that we need to know",
+            "target": "Dinos todo lo que necesitamos saber (alergias, etc...)"
+        }, 
+        {
+            "source": "Do you need a ride?",
+            "target": "Necesitas que te llevemos?"
+        },
+        {
+            "source": "Coming with us?",
+            "target": "Vendrás a nuestro día?"
+        },
+        {
+            "source": "Yes",
+            "target": "Si"
+        },
+        {
+            "source": "Reservation Form",
+            "target": "Formulario de asistencia"
+        },
+        {
+            "source": "First Name",
+            "target": "Nombre"
+        }
+    ]
+};
